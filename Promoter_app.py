5bcdb9aefac6a95a749421d625d85cece3aa9020
Shuaib <113611183+Shuaib21803@users.noreply.github.com>
2024-06-10 12:17:45 +0530

    Update Promoter_app.py

--- Promoter_app.py
+++ Promoter_app.py
@@ -5,8 +5,8 @@ import requests
 st.set_page_config(layout="wide")
 
 df = pd.read_csv('https://raw.githubusercontent.com/Shuaib21803/Promoter-Screener/main/stocks.csv')
-#meta = requests.get('https://api.github.com/repos/Shuaib21803/Promoter-Screener/commits?path=stocks.csv').json()[0]['commit']['committer']['date']
-#meta = ' '.join(meta.split('T'))[0:-1]
+meta = requests.get('https://api.github.com/repos/Shuaib21803/Promoter-Screener/commits?path=stocks.csv').json()[0]['commit']['committer']['date']
+meta = ' '.join(meta.split('T'))[0:-1]
 df['Pledged'] = df['Pledged'].str.lstrip()
 df = df[df["Pledged"].isin(['-','0.00','0'])]
 df = df[df['SAST Regulations']=='Yes']
@@ -20,4 +20,4 @@ st.subheader('Raw Data')
 #st.markdown(st_df.to_markdown(index=False))
 #st.table(st_df)
 st.dataframe(st_df)
-#st.write("Last updated:",meta)
+st.write("Last updated:",meta)
